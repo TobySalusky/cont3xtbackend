34c6ad189b608f380d57d17dbd2c0dc08595be80
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2021-06-18 13:02:57 -0400

    gitignore and start of email/phone verification

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 
-app.get('/whoisdomain', async (req, res) => {
+app.get('/who-is-domain', async (req, res) => {
 
     try {
         const whois = require('whois-json');
@@ -37,6 +37,57 @@ app.get('/ip2asn', async (req, res) => {
         res.json(results)
     });
 })
+
+app.get('/verify-email', async (req, res) => {
+    
+    const verifier = require('email-verify');
+    const infoCodes = verifier.verifyCodes;
+    
+    verifier.verify( req.query.email, function( err, info ){
+        if( err ) {
+            console.log(err);
+            res.send('err')
+        } else{
+            console.log( "Success (T/F): " + info.success );
+            console.log( "Info: " + info.info );
+            
+            //Info object returns a code which representing a state of validation:
+            
+            //Connected to SMTP server and finished email verification
+            console.log(info.code === infoCodes.finishedVerification);
+            
+            //Domain not found
+            console.log(info.code === infoCodes.domainNotFound);
+            
+            //Email is not valid
+            console.log(info.code === infoCodes.invalidEmailStructure);
+            
+            //No MX record in domain name
+            console.log(info.code === infoCodes.noMxRecords);
+            
+            //SMTP connection timeout
+            console.log(info.code === infoCodes.SMTPConnectionTimeout);
+            
+            //SMTP connection error
+            console.log(info.code === infoCodes.SMTPConnectionError)
+            
+            res.json(info)
+        }
+    });
+    
+})
+
+app.get('/verify-phone-number', async (req, res) => {
+    
+    console.log('test')
+    console.log(req.query.phoneNumber)
+    
+    const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+    const number = phoneUtil.parse(req.query.phoneNumber, 'US');
+    
+    res.send(phoneUtil.isValidNumber(number))
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 module.exports = app
